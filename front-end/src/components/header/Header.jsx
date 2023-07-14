ab3276c82412301f4c33927968c64eeb2c7e3afb
kushandesilva59 <kushandesilva801@gmail.com>
2023-07-14 22:47:45 +0530

    add routes to front-end

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "../header/header-styles.css"
 import {Form} from "../form/Form"
 import logo from '../../images/Resident_Logo.png'
+import { Link } from 'react-router-dom'
 
 
 export const Header = () => {
@@ -13,8 +14,8 @@ export const Header = () => {
             <img src={logo} alt="logo" className="logo" />
 
             <ul className='list'>
-                <li>Home</li>
-                <li>Items</li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">Items</Link></li>
                 <button className='loginBtn'>Login</button>
             </ul>
         </nav>
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider,Route}from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<div>
+      <App/>
+    </div>,
+  },
+  {
+    path:"/home",
+    element:<div>Hello Home!</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode> 
+
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
