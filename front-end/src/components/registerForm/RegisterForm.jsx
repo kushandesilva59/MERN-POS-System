9785948fb19580b54957ac35286d3ef39deb0272
kushandesilva59 <kushandesilva801@gmail.com>
2023-07-24 15:17:27 +0530

    update

--- front-end/src/components/registerForm/RegisterForm.jsx
+++ front-end/src/components/registerForm/RegisterForm.jsx
@@ -9,6 +9,7 @@ export const Register = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
@@ -32,11 +33,24 @@ export const Register = (props) => {
                 axios.get("http://localhost:8000/customer/lastCustomer")
                 .then(response =>{
                     console.log(response.data[0].cusId);
-                })
+
+                    let cusId = response.data[0].cusId;
+                    const newId = ++cusId;
+
+                    const customer ={
+                        "cusId" : newId,
+                        "cusName" : name,
+                        "cusAddress": address,
+                        "cusEmail": email,
+                        "cusPassword":password
+                    }
+
+                    axios.post("http://localhost:8000/customer/save",customer)
+                });
               
 
 
-                navigate('/products')
+                navigate('/products');
                 
 
                 
@@ -61,9 +75,13 @@ export const Register = (props) => {
                     <label htmlFor="email">Email</label>
                     <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
 
+                    <label htmlFor="address">Address</label>
+                    <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder="Address" id="address" name="address" />
 
                     <label htmlFor="password">Password</label>
                     <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" id="password" name="password" />
+
+                   
 
                     <button type="submit" onClick={registerCustomer}>Log In</button>
                 </form>
